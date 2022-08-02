737221139088afbd5302db7c9185f3ca4116d550
Rysent <735134025@qq.com>
2022-08-03 00:24:29 +0800

    1

--- server/main.go
+++ server/main.go
@@ -13,5 +13,6 @@ func main() {
 			"message": "pong",
 		})
 	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
---
+++ server/service/UserService.go
@@ -0,0 +1,9 @@
+package service
+
+type UserService struct{}
+
+func (u *UserService) Login() {
+	{
+
+	}
+}
