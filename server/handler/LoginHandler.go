b40a15b02f48134c350e3226328f698e0e73fca2
mxdzzy11 <mxdzzy11@gmail.com>
2022-08-03 09:52:57 +0800

    add login todo list

--- server/handler/LoginHandler.go
+++ server/handler/LoginHandler.go
@@ -9,8 +9,18 @@ func Login(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "pong",
 	})
+	// TODO : login type  0: passwd  1: token and cookie (without passwd )
+	// TODO : if login type == 0 , get userName and passwd ,check the db ,  if pass return token and cookie (save the token to redis with 1 day )
 }
 
 func LogOut() {
+	// todo : delete the user's token in redis
+}
 
+func ForgetPasswd() {
+	// todo: you know
+}
+
+func CheckToken() {
+	// todo: you know
 }
