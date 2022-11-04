567560092606baa3380cf37e8a882d9692a28ff7
zz <735134025@qq.com>
2022-11-04 15:06:10 +0800

    哦？

--- server/initialize/conf.go
+++ server/initialize/conf.go
@@ -1,12 +1,22 @@
 package initialize
 
-import "github.com/spf13/viper"
+import (
+	"fhgo/config"
+	"fhgo/global"
+	"github.com/spf13/viper"
+)
 
 func InitConfig() {
+	c := config.Conf{}
 	v := viper.New()
 	v.AddConfigPath(".")
 	v.SetConfigType("yaml")
 	v.SetConfigName("config.yaml")
+	err := v.Unmarshal(&c)
+	if err != nil {
+		return
+	}
+	global.Config = &c
 
 	//viper.SetConfigName("config.yaml")
 }
